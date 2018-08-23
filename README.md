6e6f31e3df971beec9402a34bc746f7bc6aaa3e9
Wink Saville <wink@saville.com>
2018-08-23 15:39:55 -0700

    Cleanup

--- README.md
+++ README.md
@@ -1,10 +1,38 @@
 # Zig benchmark
 
-Explore how to make a micro benchmark.
+Explore how to make a micro benchmark in zig.
 
-A benchmark is a sturct with fn's init, setup, tearDown and benchmark:
+A benchmark framework very very loosely based on
+[google/benchmark](https://github.com/google/benchmark).
+
+Here is some information from intel, but after looking
+at google/benchmark it appears they aren't directly using
+these techniques so for now I'm just using zig's Timer.
+
+[Benchmark information for X86 from Intel]
+  (https://www.intel.com/content/dam/www/public/us/en/documents/white-papers/ia-32-ia-64-benchmark-code-execution-paper.pdf)
+[Intel 64 and IA-32 ARchitectures SDM]
+  (https://www.intel.com/content/www/us/en/architecture-and-technology/64-ia-32-architectures-software-developer-manual-325462.html)
+
+## Introduction
+A benchmark is a struct with fn's init, setup, benchmark and tearDown.
+- init is called to create an instance.
+ - returns Self
+- setup is called once before benchmark is called.
+ - May return void or !void
+- benchmark is called for each iteration.
+ - May return void or !void
+- tearDown is called after all iterations.
+ - May return void or !void
+
+## Example
+
+To keep the fn benchmark from being optimized to nothing
+I'm using volatile pointers. There maybe better solutions
+but this kinda works.
 ```
 test "benchmark.add" {
+    // Our benchmark
     const BmAdd = struct {
         const Self = this;
 
@@ -12,20 +40,23 @@ test "benchmark.add" {
         b: u64,
         r: u128,
 
+        // Initialize Self
         fn init() Self {
             return Self {
                 .a = undefined, .b = undefined, .r = undefined,
             };
         }
 
+        // Setup prior to the first call to Self.benchmark, may return void or !void
         fn setup(pSelf: *Self) !void {
             var timer = try Timer.start();
             const DefaultPrng = std.rand.DefaultPrng;
             var prng = DefaultPrng.init(timer.read());
-            pSelf.a = prng.random.range(u64, 0, 10000000);
-            pSelf.b = prng.random.range(u64, 0, 10000000);
+            pSelf.a = prng.random.scalar(u64);
+            pSelf.b = prng.random.scalar(u64);
         }
 
+        // Called for ever iteration of the benchmark
         fn benchmark(pSelf: *Self) void {
             var pA: *volatile u64 = &pSelf.a;
             var pB: *volatile u64 = &pSelf.b;
@@ -33,44 +64,51 @@ test "benchmark.add" {
             pR.* = pA.* + pB.*;
         }
 
-        fn tearDown(pSelf: *Self) void {
+        // TearDown called after the last call to Self.benchmark, may return void or !void
+        fn tearDown(pSelf: *Self) !void {
+            if (pSelf.r != pSelf.a + pSelf.b) return error.Failed;
         }
     };
 
+    // Create an instance of the framework and optionally change min_runtime_ns
     var bf = BenchmarkFramework.init();
+    //bf.min_runtime_ns = ns_per_s * 3;
+    bf.logl = 1;
+
+    // Since this is a test print a \n before we run
+    warn("\n");
+
+    // Run the benchmark
     try bf.run(BmAdd);
 }
 ```
 
-To keep the benchmark from being optimized to nothing
-I'm using volatile pointers. There maybe better solutions
-but this kinda works.
-
 ## Test on my desktop debug
 ```bash
 $ time zig test --release-fast benchmark.zig
-Test 1/1 benchmark.add...runIterations: iterations:1 runtime:0.000s ns/op:74ns
-runIterations: iterations:1000 runtime:0.000s ns/op:1ns
-runIterations: iterations:10000 runtime:0.000s ns/op:1ns
-runIterations: iterations:100000 runtime:0.000s ns/op:1ns
-runIterations: iterations:1000000 runtime:0.001s ns/op:1ns
-runIterations: iterations:10000000 runtime:0.010s ns/op:1ns
-runIterations: iterations:100000000 runtime:0.062s ns/op:1ns
-runIterations: iterations:140000000 runtime:0.086s ns/op:1ns
-runIterations: iterations:196000000 runtime:0.121s ns/op:1ns
-runIterations: iterations:274400000 runtime:0.158s ns/op:1ns
-runIterations: iterations:384160000 runtime:0.218s ns/op:1ns
-runIterations: iterations:537824000 runtime:0.306s ns/op:1ns
-runIterations: iterations:752953600 runtime:0.428s ns/op:1ns
-runIterations: iterations:1054135040 runtime:0.595s ns/op:1ns
-
-iterations:1054135040 runtime:0.595s ns/op:1ns
+Test 1/1 benchmark.add...
+run: logl=1 min_runtime_ns=500000000 max_iterations=100000000000
+iterations:1 runtime:0.000s ns/op:95.000ns
+iterations:1000 runtime:0.000s ns/op:1.558ns
+iterations:10000 runtime:0.000s ns/op:1.444ns
+iterations:100000 runtime:0.000s ns/op:1.972ns
+iterations:1000000 runtime:0.001s ns/op:1.471ns
+iterations:10000000 runtime:0.013s ns/op:1.336ns
+iterations:100000000 runtime:0.061s ns/op:0.611ns
+iterations:140000000 runtime:0.085s ns/op:0.610ns
+iterations:196000000 runtime:0.120s ns/op:0.610ns
+iterations:274400000 runtime:0.156s ns/op:0.567ns
+iterations:384160000 runtime:0.218s ns/op:0.566ns
+iterations:537824000 runtime:0.303s ns/op:0.563ns
+iterations:752953600 runtime:0.424s ns/op:0.563ns
+iterations:1054135040 runtime:0.595s ns/op:0.565ns
+iterations:1054135040 runtime:0.595s ns/op:0.565ns
 OK
 All tests passed.
 
-real	0m5.532s
-user	0m5.419s
-sys	0m0.101s
+real	0m5.518s
+user	0m5.407s
+sys	0m0.095s
 ```
 
 ## Clean
--- benchmark.zig
+++ benchmark.zig
@@ -1,11 +1,15 @@
+// A benchmark framework very very loosely based on
+// [google/benchmark](https://github.com/google/benchmark).
+//
+// Here is some information from intel, but after looking
+// at google/benchmark it appears they aren't directly using
+// these techniques so for now I'm just using zig's Timer.
+// 
 // [Benchmark information for X86 from Intel]
 //   (https://www.intel.com/content/dam/www/public/us/en/documents/white-papers/ia-32-ia-64-benchmark-code-execution-paper.pdf)
-//
 // [Intel 64 and IA-32 ARchitectures SDM]
 //   (https://www.intel.com/content/www/us/en/architecture-and-technology/64-ia-32-architectures-software-developer-manual-325462.html)
 //
-// [google/benchmark]
-//   (https://github.com/google/benchmark)
 
 const builtin = @import("builtin");
 const TypeInfo = builtin.TypeInfo;
@@ -19,9 +23,6 @@ const warn = std.debug.warn;
 const assert = std.debug.assert;
 
 const ns_per_s = 1000000000;
-
-const BenchmarkState = struct {
-};
 
 /// mfence instruction
 fn mfence() void {
@@ -42,13 +43,15 @@ fn sfence() void {
 const BenchmarkFramework = struct {
     const Self = this;
 
-    min_runtime_ns: u64,
-    max_iterations: u64,
+    pub logl: usize,
+    pub min_runtime_ns: u64,
+    pub max_iterations: u64,
     timer: Timer,
 
     /// Initialize benchmark framework
     pub fn init() Self {
         return Self {
+            .logl = 0,
             .min_runtime_ns = ns_per_s / 2,
             .max_iterations = 100000000000,
             .timer = undefined,
@@ -59,28 +62,30 @@ const BenchmarkFramework = struct {
     pub fn run(pSelf: *Self, comptime T: type) !void {
         var run_time: u64 = 0;
         var iterations: u64 = 1;
-        var state = BenchmarkState {};
+
+        if (pSelf.logl >= 1)
+            warn("run: logl={} min_runtime_ns={} max_iterations={}\n",
+                    pSelf.logl, pSelf.min_runtime_ns, pSelf.max_iterations);
 
         // Make sure T is a struct
         const info = @typeInfo(T);
         if (TypeId(info) != TypeId.Struct) return error.T_NotStruct;
 
-        // It has to have an init which returns Self
+        // Create a benchmark struct, it has to have an init which returns Self
         var bm = T.init();
 
         // Call bm.setup with try if needed
-        //if (comptime getFnReturnType("setup", info.Struct.defs) == void) {
         if (comptime @typeOf(T.setup).ReturnType == void) {
             bm.setup();
         } else {
             try bm.setup();
         }
 
-        while (iterations < pSelf.max_iterations) {
-            run_time = try runIterations(pSelf, T, &bm, iterations);
-            //warn("run_time={} min_runtime_ns={}\n", run_time, pSelf.min_runtime_ns);
+        // Loop until iterations == max_iterations or run_time >= min_runtime_ns
+        while (iterations <= pSelf.max_iterations) {
+            run_time = try pSelf.runIterations(T, &bm, iterations);
+            if (pSelf.logl >= 1) pSelf.report(run_time, iterations);
             if ((run_time >= pSelf.min_runtime_ns) or (iterations >= pSelf.max_iterations)) {
-                //warn("done\n");
                 break;
             } else {
                 var denom: u64 = undefined;
@@ -99,31 +104,19 @@ const BenchmarkFramework = struct {
                 if (iterations > pSelf.max_iterations) {
                     iterations = pSelf.max_iterations;
                 }
-                //warn("iteratons:{} numer:{} denom:{}\n", iterations, numer, denom);
+                if (pSelf.logl >= 2) warn("iteratons:{} numer:{} denom:{}\n", iterations, numer, denom);
             }
         }
+
         // Call bm.tearDown with try if needed
-        //if (comptime getFnReturnType("tearDown", info.Struct.defs) == void) {
         if (comptime @typeOf(T.tearDown).ReturnType == void) {
             bm.tearDown();
         } else {
             try bm.tearDown();
         }
 
-        warn("\niterations:{} runtime:{.3}s ns/op:{.0}ns\n",
-            iterations,
-            @intToFloat(f64, run_time)/@intToFloat(f64, ns_per_s),
-            @intToFloat(f64, run_time)/@intToFloat(f64, iterations),
-        );
-    }
-
-    fn getFnReturnType(name: [] const u8, defs: []TypeInfo.Definition) type {
-        for (defs) |def| {
-            if (std.mem.eql(u8, def.name, name)) {
-                return def.data.Fn.return_type;
-            }
-        }
-        return error.NoReturnType;
+        // Report the results
+        pSelf.report(run_time, iterations);
     }
 
     /// Run the specified number of iterations returning the time in ns
@@ -131,40 +124,33 @@ const BenchmarkFramework = struct {
         pSelf: *Self,
         comptime T: type,
         pBm: *T,
-        //pState: *BenchmarkState,
         iterations: u64,
     ) !u64 {
         const info = @typeInfo(T);
 
-        //warn("runIterations: iteratons={}\n", iterations);
         var timer = try Timer.start();
         var iter = iterations;
         while (iter > 0) : (iter -= 1) {
-            // Call bm.setup with try if needed
-            //lfence();
-            //@fence(AtomicOrder.Acquire); // Generates no type of fence, expected lfence
-            if (comptime getFnReturnType("benchmark", info.Struct.defs) == void) {
+            if (comptime @typeOf(T.benchmark).ReturnType == void) {
                 pBm.benchmark();
             } else {
                 try pBm.benchmark();
             }
-            //sfence();
-            //@fence(AtomicOrder.Release); // Generates no type of fence, expected sfence
-            //@fence(AtomicOrder.AcqRel); // Generates no type of fence, expected ??
-            //@fence(AtomicOrder.SeqCst); // Generates mfence
         }
-        var time = timer.read();
-        warn("runIterations: iterations:{} runtime:{.3}s ns/op:{.0}ns\n",
+        return timer.read();
+    }
+
+    fn report(pSelf: *Self, run_time_ns: u64, iterations: u64) void {
+        warn("iterations:{} runtime:{.3}s ns/op:{.3}ns\n",
             iterations,
-            @intToFloat(f64, time)/@intToFloat(f64, ns_per_s),
-            @intToFloat(f64, time)/@intToFloat(f64, iterations),
+            @intToFloat(f64, run_time_ns)/@intToFloat(f64, ns_per_s),
+            @intToFloat(f64, run_time_ns)/@intToFloat(f64, iterations),
         );
-        return time;
     }
 };
 
-
 test "benchmark.add" {
+    // Our benchmark
     const BmAdd = struct {
         const Self = this;
 
@@ -172,31 +158,51 @@ test "benchmark.add" {
         b: u64,
         r: u128,
 
+        // Initialize Self
         fn init() Self {
             return Self {
                 .a = undefined, .b = undefined, .r = undefined,
             };
         }
 
+        // Setup prior to the first call to Self.benchmark, may return void or !void
         fn setup(pSelf: *Self) !void {
             var timer = try Timer.start();
             const DefaultPrng = std.rand.DefaultPrng;
             var prng = DefaultPrng.init(timer.read());
-            pSelf.a = prng.random.range(u64, 0, 10000000);
-            pSelf.b = prng.random.range(u64, 0, 10000000);
+            pSelf.a = prng.random.scalar(u64);
+            pSelf.b = prng.random.scalar(u64);
         }
 
+        // Called on every iteration of the benchmark, may return void or !void
         fn benchmark(pSelf: *Self) void {
+            //lfence();
+            //@fence(AtomicOrder.Acquire); // Generates no type of fence, expected lfence
             var pA: *volatile u64 = &pSelf.a;
             var pB: *volatile u64 = &pSelf.b;
             var pR: *volatile u128 = &pSelf.r;
             pR.* = pA.* + pB.*;
+            //sfence();
+            //@fence(AtomicOrder.Release); // Generates no type of fence, expected sfence
+            //@fence(AtomicOrder.AcqRel); // Generates no type of fence, expected ??
+            //@fence(AtomicOrder.SeqCst); // Generates mfence
+            //mfence();
         }
 
-        fn tearDown(pSelf: *Self) void {
+        // TearDown called after the last call to Self.benchmark, may return void or !void
+        fn tearDown(pSelf: *Self) !void {
+            if (pSelf.r != pSelf.a + pSelf.b) return error.Failed;
         }
     };
 
+    // Create an instance of the framework and optionally change min_runtime_ns
     var bf = BenchmarkFramework.init();
+    //bf.min_runtime_ns = ns_per_s * 3;
+    bf.logl = 1;
+
+    // Since this is a test print a \n before we run
+    warn("\n");
+
+    // Run the benchmark
     try bf.run(BmAdd);
 }
